6ab1de57ac9f169ebe9141ee88f7de4c651932fd
SsandySANTHOSH <santhoshs4717@gmail.com>
2023-07-22 21:12:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { useState } from "react";
-// import logo from './logo.svg';
-import './App.css';
-import { Login } from "./Login";
-import { Register } from "./Register";
+import Home from "./components/Home/Home";
+import SavedBooks from "./components/SavedBooks/SavedBooks";
+import ViewBook from "./components/SavedBooks/ViewBook";
+import Footer from "./components/Footer";
+import Missing from "./components/Missing";
+import NewBook from "./components/NewBooks/NewBook";
+import Book from "./components/EditBook/Book";
+import EditBook from "./components/EditBook/EditBook";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import { DataProvider } from "./context/DataContext";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
     <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+      <DataProvider>
+        <Header title="Library" hub=" Hub " />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="book">
+            <Route index element={<NewBook />} />
+            <Route path=":id" element={<Book />} />
+          </Route>
+          <Route path="/edit/:id" element={<EditBook />} />
+          <Route path="savedbooks" element={<SavedBooks />} />
+          <Route path="savedbooks/book/:id" element={<ViewBook />} />
+          <Route path="*" element={<Missing />} />
+        </Routes>
+        <Footer />
+      </DataProvider>
     </div>
   );
 }
